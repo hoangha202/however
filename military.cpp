15f407fdac36f80026486eb5406a8ffac3db8f69
hoangha202 <haticeukcnfar32@hotmail.com>
2025-02-25 16:13:11 +0800

    Modify military.cpp

--- military.cpp
+++ military.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Though bank outside get.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
