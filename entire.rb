7c0fef44674f865f63acdafdc77230007f9c5136
hoangha202 <haticeukcnfar32@hotmail.com>
2025-03-26 13:55:00 +0800

    chore(core): maintain entire file

--- entire.rb
+++ entire.rb
@@ -1,1 +1,13 @@
-puts "Hello, World!"
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# A simple Hello World program in Ruby
+class Greeter
+  def self.say_hello(name = "World")
+    puts "Hello, #{name}!"
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  Greeter.say_hello
+end
