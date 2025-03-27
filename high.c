09bad62dc0862338e4c729ee89f812ea6d32faa8
hoangha202 <haticeukcnfar32@hotmail.com>
2025-03-27 18:52:27 +0800

    style(core): format code in high.c

--- high.c
+++ high.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main() {
-	printf("Hello, World!\n");
-	return 0;
+
+/**
+ * @brief Main function that prints "Hello, World!" to standard output
+ * @return Returns 0 on successful execution
+ */
+int main(void) {
+    // Print greeting message followed by newline
+    printf("Hello, World!\n");
+    
+    return 0;
 }
